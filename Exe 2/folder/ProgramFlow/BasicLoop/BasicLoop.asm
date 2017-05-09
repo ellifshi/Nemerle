a0a2daab4359815da29e1820f5afe41744b14cf2
shneor <had5678@gmail.com>
2017-05-09 17:38:13 +0300

    delete all the unneccery asm file (if we have some asm files and we merge him to one file, and deltet asm file BEFURE to run the program, if we not delete it mabay append one file to another

--- Exe 2/folder/ProgramFlow/BasicLoop/BasicLoop.asm
+++ Exe 2/folder/ProgramFlow/BasicLoop/BasicLoop.asm
@@ -26,7 +26,7 @@ M=D
 //End pop local 0
 
 //Start label LOOP_START
-(LOOP_START)
+(LOOP_START_BasicLoop)
 //End label LOOP_START
 
 //Start push argument 0
@@ -159,14 +159,197 @@ M=D
 M=M+1
 //End push argument 0
 
-//Start ifGoto Exe_2.Analyzer2+_N_static_proxy_6517
+//Start ifGoto Exe_2.Analyzer2+_N_static_proxy_6523
 @SP
 M=M-1
 A=M
 D=M
-@LOOP_START
+@LOOP_START_BasicLoop
 D;JMP
-//End ifGoto Exe_2.Analyzer2+_N_static_proxy_6517
+//End ifGoto Exe_2.Analyzer2+_N_static_proxy_6523
+
+//Start push local 0
+@0
+D=A
+@LCL
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+//End push local 0
+
+//Start push constant 0
+@0
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+//End push constant 0
+
+//Start pop local 0
+@SP
+M=M-1
+@0
+D=A
+@LCL
+D=M+D
+@R13
+M=D
+@SP
+A=M
+D=M
+@R13
+A=M
+M=D
+//End pop local 0
+
+//Start label LOOP_START
+(LOOP_START_BasicLoop)
+//End label LOOP_START
+
+//Start push argument 0
+@0
+D=A
+@ARG
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+//End push argument 0
+
+//Start push local 0
+@0
+D=A
+@LCL
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+//End push local 0
+
+//Start add
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+A=M
+D=D+A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+//End add
+
+//Start pop local 0	
+@SP
+M=M-1
+@0	
+D=A
+@LCL
+D=M+D
+@R13
+M=D
+@SP
+A=M
+D=M
+@R13
+A=M
+M=D
+//End pop local 0	
+
+//Start push argument 0
+@0
+D=A
+@ARG
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+//End push argument 0
+
+//Start push constant 1
+@1
+D=A
+@SP
+A=M
+M=D
+@SP
+M=M+1
+//End push constant 1
+
+//Start sub
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+A=M
+D=A-D
+@SP
+A=M
+M=D
+@SP
+M=M+1
+//End sub
+
+//Start pop argument 0
+@SP
+M=M-1
+@0
+D=A
+@ARG
+D=M+D
+@R13
+M=D
+@SP
+A=M
+D=M
+@R13
+A=M
+M=D
+//End pop argument 0
+
+//Start push argument 0
+@0
+D=A
+@ARG
+A=M+D
+D=M
+@SP
+A=M
+M=D
+@SP
+M=M+1
+//End push argument 0
+
+//Start ifGoto Exe_2.Analyzer2+_N_static_proxy_6523
+@SP
+M=M-1
+A=M
+D=M
+@LOOP_START_BasicLoop
+D;JMP
+//End ifGoto Exe_2.Analyzer2+_N_static_proxy_6523
 
 //Start push local 0
 @0
